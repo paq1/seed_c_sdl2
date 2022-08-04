12ea502a99ff7aa5136b2389276f448d55197863
paq1 <32450977+paq1@users.noreply.github.com>
2022-08-04 09:54:06 +0200

    Gestion evenement (#3)
    * feat: ajout evenement quit et clavier
    
    * feat: event fenetre et event windows
    
    * ajout de la lib sdl tff

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 ### Seed SDL2 
 ## SETUP
 # Windows
+```
+mingw32-make -f Makefile run
+```
 # Linux
 Pas d'info
 # MacOS
--- src/main.c
+++ src/main.c
@@ -1,7 +1,12 @@
 #include <SDL.h>
+#include <SDL_ttf.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct position_t {
+    double x;
+    double y;
+} position_t;
 
 void SDL_ExitWithError(const char *message);
 void affiche_version_sdl();
@@ -12,6 +17,10 @@ int main(int argc, char* argv[]) {
 
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_ExitWithError("Initialisation SDL");
+    }
+
+    if (TTF_Init() != 0) {
+        SDL_ExitWithError("Initialisation TTF");
     }
 
     SDL_Window *window = create_default_window();
@@ -25,46 +34,131 @@ int main(int argc, char* argv[]) {
         SDL_ExitWithError("Initialisation renderer");
     }
 
-    SDL_Surface *surface = SDL_LoadBMP("assets/sprites/facture_gaz_face.bmp");
-    if (surface == NULL) {
+    /***************************************************/
+    SDL_bool program_launched = SDL_TRUE;
+
+    unsigned int old_ticks = SDL_GetTicks();
+    double dt = 0.0;
+
+    TTF_Font * font = TTF_OpenFont("assets/fonts/dpcomic.ttf", 25);
+    SDL_Color color = { 255, 255, 255 };
+    SDL_Surface * surface_text = TTF_RenderText_Solid(font, "fps : ???", color);
+    SDL_Texture * texture_text = SDL_CreateTextureFromSurface(renderer, surface_text);
+    SDL_FreeSurface(surface_text);
+
+    SDL_Surface *surfaceSmiley = SDL_LoadBMP("assets/sprites/smiley_sdl_seed.bmp");
+    if (surfaceSmiley == NULL) {
+        TTF_CloseFont(font);
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_ExitWithError("Chargement image");
     }
 
-    SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-    SDL_FreeSurface(surface);
+    SDL_Texture *textureSmiley = SDL_CreateTextureFromSurface(renderer, surfaceSmiley);
+    SDL_FreeSurface(surfaceSmiley);
 
-    if (texture == NULL) {
+    if (textureSmiley == NULL) {
+        TTF_CloseFont(font);
         SDL_DestroyRenderer(renderer);
         SDL_DestroyWindow(window);
         SDL_ExitWithError("Creer texture");
     }
 
-    SDL_Rect rect = {200, 200, 100, 100};
+    position_t position = {0, 0};
 
-    if (SDL_QueryTexture(texture, NULL, NULL, &rect.w, &rect.h) != 0) {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Charger texture");
+    while (program_launched) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+
+            switch (event.type) {
+                case SDL_WINDOWEVENT:
+                    if (event.window.event == SDL_WINDOWEVENT_LEAVE) {
+                        SDL_Log("La souris est sortie de la fenêtre.\n");
+                    }
+                    if (event.window.event == SDL_WINDOWEVENT_ENTER) {
+                        SDL_Log("La souris est entrée dans la fenêtre.\n");
+                    }
+                    if (event.window.event == SDL_WINDOWEVENT_FOCUS_GAINED) {
+                        SDL_Log("La fenêtre a le focus.\n");
+                    }
+                    if (event.window.event == SDL_WINDOWEVENT_FOCUS_LOST) {
+                        SDL_Log("La fenêtre n'a plus le focus.\n");
+                    }
+                    break;
+                case SDL_MOUSEMOTION:
+                    // SDL_Log("Mouse moved to %d,%d\n", event.motion.x, event.motion.y);
+                    break;
+                case SDL_MOUSEBUTTONDOWN:
+                    // SDL_Log("Mouse moved to %d,%d\n", event.motion.x, event.motion.y);
+                    if (event.button.button == SDL_BUTTON_LEFT) {
+                        SDL_Log("Left mouse button down\n");
+                    } else if (event.button.button == SDL_BUTTON_RIGHT) {
+                        SDL_Log("Right mouse button down\n");
+                    } else if (event.button.button == SDL_BUTTON_MIDDLE) {
+                        SDL_Log("Middle mouse button down\n");
+                    }
+                    SDL_Log("Mouse clicked to %d,%d\n", event.motion.x, event.motion.y);
+                    break;
+                case SDL_QUIT:
+                    program_launched = SDL_FALSE;
+                    break;
+                case SDL_KEYDOWN:
+                    if (event.key.keysym.sym == SDLK_ESCAPE) {
+                        program_launched = SDL_FALSE;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        unsigned int new_ticks = SDL_GetTicks();
+        dt = (new_ticks - old_ticks) / 1000.0;
+        old_ticks = new_ticks;
+
+        position.x += dt * 200.0;
+        position.y += dt * 100.0;
+
+        
+        SDL_SetRenderDrawColor(renderer, 255, 100, 100, 255);
+
+
+        if (SDL_RenderCopy(renderer, textureSmiley, NULL, &(SDL_Rect){position.x, position.y, 64, 64}) != 0) {
+            TTF_CloseFont(font);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_ExitWithError("Afficher texture");
+        }
+
+        SDL_Rect rect = {0, 0, 0, 0};
+
+        if (SDL_QueryTexture(texture_text, NULL, NULL, &rect.w, &rect.h) != 0) {
+            TTF_CloseFont(font);
+            SDL_DestroyTexture(texture_text);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_ExitWithError("Charger texture");
+        }
+
+        if (SDL_RenderCopy(renderer, texture_text, NULL, &rect) != 0) {
+            TTF_CloseFont(font);
+            SDL_DestroyTexture(texture_text);
+            SDL_DestroyRenderer(renderer);
+            SDL_DestroyWindow(window);
+            SDL_ExitWithError("Afficher texture texte");
+        }
+
+
+        SDL_RenderPresent(renderer);
+        if (SDL_RenderClear(renderer) != 0) SDL_ExitWithError("Effacement renderer");
     }
-
-    if (SDL_RenderCopy(renderer, texture, NULL, &rect) != 0) {
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_ExitWithError("Afficher texture");
-    }
-
-    SDL_RenderPresent(renderer);
-
-    // if (SDL_RenderClear(renderer) != 0) SDL_ExitWithError("Effacement renderer");
-    
-
-    SDL_Delay(3000); // todo delete this line
-
-    SDL_DestroyTexture(texture);
+    /***************************************************/
+    TTF_CloseFont(font);
+    SDL_DestroyTexture(texture_text);
+    SDL_DestroyTexture(textureSmiley);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
+    TTF_Quit();
     SDL_Quit();
 
     return EXIT_SUCCESS;
@@ -73,6 +167,7 @@ int main(int argc, char* argv[]) {
 void SDL_ExitWithError(const char *message)
 {
     SDL_Log("ERREUR : %s > %s\n", message, SDL_GetError());
+    TTF_Quit();
     SDL_Quit();
     exit(EXIT_FAILURE);
 }
